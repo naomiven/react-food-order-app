cce77b09742161f211b25b47f160700971c88ca1
naovqz <naomi.mira@gmail.com>
2022-04-24 16:08:37 -0700

    add CartContext to manage cart data

---
+++ src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import CartContext from './cart-context';
+
+/* Manages cart context data & provide context to all
+components that want access to it */
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
